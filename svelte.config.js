09dc5e18a87fc90aba01fb496f8bd04f9423ae42
Dan Sack <dsack7@gmail.com>
2022-01-20 18:52:23 +0100

    fix: static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 // import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
